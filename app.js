324d176eeb04bc25eaa302bc45d95a567b0167fd
Mapple <mapplezq@gmail.com>
2021-01-11 16:32:58 +0800

    add Procfile and health check in yml

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('0.0.1')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
